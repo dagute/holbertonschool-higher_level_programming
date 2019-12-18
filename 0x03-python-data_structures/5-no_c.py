cb9366b3f4f98f7967109e261f169372c7868975
dagute <1271@holbertonschool.com>
2019-12-18 00:02:35 +0000

    Can you C me now

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+
+def no_c(my_string):
+    new_word = ""
+    for word in my_string:
+        if word != 'c' and word != 'C':
+            new_word += word
+    return new_word
