353b8fc81c6f8e87ba3b29b26d3e8ac0a92ab95e
dagute <1271@holbertonschool.com>
2019-12-18 00:12:04 +0000

    Can you C me now

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,7 +3,7 @@
 
 def no_c(my_string):
     new_word = ''
-    for word in my_string:
-        if word != 'c' and word != 'C':
-            new_word += word
+    for word in range(len(my_string)):
+        if my_string[word] != 'c' and my_string[word] != 'C':
+            new_word += my_string[word]
     return new_word
