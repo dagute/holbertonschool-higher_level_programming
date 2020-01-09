b9c069f71f5ceb0d5e3d9c9a75fb8fad973988d4
dagute <1271@holbertonschool.com>
2020-01-09 04:32:11 +0000

    Safe integer print with error message

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import sys
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+    except (ValueError, TypeError) as err_msg:
+        print("Exception: {}".format(err_msg), file=sys.stderr)
+        return False
+    return True
