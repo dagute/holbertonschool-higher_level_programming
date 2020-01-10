4fdf63f9d70ab19ca2dbf47ea2958a1c9609f5ba
dagute <1271@holbertonschool.com>
2020-01-10 01:18:02 +0000

    Access and update private attribute

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -13,7 +13,8 @@ class Square:
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
-        self.__Size = value
+        else:
+            self.__size = value
 
     def area(self):
         return self.__size ** 2
