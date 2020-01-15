28a7074292281819658c957145f0775696484874
dagute <1271@holbertonschool.com>
2020-01-15 14:24:55 +0000

    A square is a rectangle

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -36,9 +36,9 @@ class Rectangle:
     def height(self, value):
         """set height"""
         if not isinstance(value, int):
-            raise TypeError("height must be an integer")
+            raise TypeError("width must be an integer")
         if value < 0:
-            raise ValueError("height must be >= 0")
+            raise ValueError("width must be >= 0")
         self.__height = value
 
     def area(self):
