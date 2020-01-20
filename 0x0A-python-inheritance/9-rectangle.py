3cefdf726ed233c2e547d0a11cc462b5ed1a39a1
dagute <1271@holbertonschool.com>
2020-01-20 22:15:46 +0000

    Full rectangle

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -15,14 +15,15 @@ class BaseGeometry:
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
 
-    class Rectangle(BaseGeometry):
-        """class Rectangle that inherits from BaseGeometry"""
-        def __init__(self, width, height):
-            """Instantiation with width and height"""
-            self.integer_validator("width", width)
-            self.integer_validator("height", height)
-            self.__width = width
-            self.__height = height
+
+class Rectangle(BaseGeometry):
+    """class Rectangle that inherits from BaseGeometry"""
+    def __init__(self, width, height):
+        """Instantiation with width and height"""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
 
     def area(self):
         """returns the rectangle area"""
