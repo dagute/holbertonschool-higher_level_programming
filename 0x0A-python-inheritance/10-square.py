06e7186e20cdb2d853104ac81b6e84f798da6cc2
dagute <1271@holbertonschool.com>
2020-01-20 22:17:38 +0000

    Square #1

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Rectangle"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Class Square"""
+    def __init__(self, size):
+        """Instantiation with size"""
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """returns the square area"""
+        return(self.__size ** 2)
