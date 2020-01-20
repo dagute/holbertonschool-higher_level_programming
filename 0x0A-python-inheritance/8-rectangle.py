9412ff5af575479a3d1497a1b3a7d60e8e774b9f
dagute <1271@holbertonschool.com>
2020-01-20 21:56:54 +0000

    Rectangle

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Rectangle"""
+
+
+class BaseGeometry:
+    """class"""
+    def area(self):
+        """raises an Exception"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """validates value"""
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+    class Rectangle(BaseGeometry):
+        """class Rectangle that inherits from BaseGeometry"""
+        def __init__(self, width, height):
+            """Instantiation with width and height"""
+            self.integer_validator("width", width)
+            self.integer_validator("height", height)
+            self.__width = width
+            self.__height = height
