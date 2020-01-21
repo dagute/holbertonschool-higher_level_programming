e04dd9744259398c60e4406073010f7634b0e36a
dagute <1271@holbertonschool.com>
2020-01-21 04:42:31 +0000

    Text indentation

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -4,12 +4,11 @@
 
 def text_indentation(text):
     """
-
     function that prints a text with 2 new lines
     after each of these characters: ., ? and :
     """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
-    s = "".join([w if w not in ".?:" else w + "\n\n" for w in text])
-    newone = "\n".join([l.strip() for l in s.split("\n")])
-    print(newone)
+    s = "".join([word if word not in ".?:"
+                 else word + "\n\n" for word in text])
+    print("\n".join([l.strip() for l in s.split("\n")]), end="")
