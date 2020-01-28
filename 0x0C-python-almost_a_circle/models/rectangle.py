d998018f7b4602a81d89e8a9ab73db764e0ffd2d
dagute <1271@holbertonschool.com>
2020-01-28 18:19:13 +0000

    update valueError msg

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -66,7 +66,7 @@ class Rectangle(Base):
         if type(value) != int:
             raise TypeError("y must be an integer")
         if value < 0:
-            raise ValueError("y must b >= 0")
+            raise ValueError("y must be >= 0")
         self.__y = value
 
     def area(self):
