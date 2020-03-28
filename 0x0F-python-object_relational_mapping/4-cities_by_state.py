a9b61d6e274b5a99c93b88e1e2c86efc4a894c02
dagute <1271@holbertonschool.com>
2020-03-28 20:45:51 +0000

    Cities by states

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -9,6 +9,7 @@ if __name__ == "__main__":
     db = MySQLdb.Connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3])
 
+    cur = db.cursor()
     cur.execute("SELECT cities.id, cities.name, states.name\
                  FROM cities\
                  INNER JOIN states ON cities.state_id = states.id\
