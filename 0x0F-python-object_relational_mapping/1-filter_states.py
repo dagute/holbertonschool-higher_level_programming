68e42975b5db4c0d71cc4a04a1ee8af0440333e9
dagute <1271@holbertonschool.com>
2020-03-28 17:50:10 +0000

    Filter states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -12,14 +12,11 @@ if __name__ == "__main__":
                          passwd=argv[2], db=argv[3], charset="utf8")
 
     cur = db.cursor()
-    cur.execute("""
-    SELECT * FROM states
-    WHERE name LIKE BYNARY 'N%'
-    ORDER BY states.id ASC
-    """)
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
 
     rows = cur.fetchall()
     for row in rows:
-        print(row)
+        if row[1][0] = 'N':
+            print(row)
     cur.close()
     db.close()
