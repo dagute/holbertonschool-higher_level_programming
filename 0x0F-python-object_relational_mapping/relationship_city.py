6808166c2f01161c30e5873a76984746726b2b0d
dagute <1271@holbertonschool.com>
2020-04-06 00:05:02 +0000

    delete declarative base

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -6,8 +6,6 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 
-Base = declarative_base()
-
 
 class City(Base):
     """City class"""
