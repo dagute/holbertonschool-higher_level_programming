32a9a7a72b43224b0c11fd5e6dd8909025b17735
dagute <1271@holbertonschool.com>
2020-04-09 18:00:30 +0000

    cURL POST parameters

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes in a URL, sends a POST request to the passed URL
-curl -s -d "$1" -X POST "email=hr@holbertonschool.com&subject=I will always be here for PLD"
+curl -sX POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" "$1"
