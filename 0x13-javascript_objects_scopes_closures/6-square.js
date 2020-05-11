6f8255a5771d6419a8fd7332924686167a7fd6cb
dagute <1271@holbertonschool.com>
2020-05-11 17:02:44 +0000

    first commit

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const squ5 = require('./5-square');
+module.exports = class Square extends squ5 {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+        console.log(c.repeat(this.width));
+    }
+  }
+};
