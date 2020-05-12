91cb041ac8ce11593bf644796cf205d7526cc54e
dagute <1271@holbertonschool.com>
2020-05-12 22:44:25 +0000

    Who was playing in this movie

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else if (response.statusCode === 200) {
+    for (const names of JSON.parse(body).characters) {
+      request.get(names, function (error, response, body) {
+        if (error) {
+          console.log(error);
+        } else if (response.statusCode === 200) {
+          console.log(JSON.parse(body).name);
+        }
+      });
+    }
+  }
+});
