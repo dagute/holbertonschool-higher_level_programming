b8cd8cc0d947e506939f03aa1d28b65a85e1b3fb
dagute <1271@holbertonschool.com>
2020-05-12 22:14:35 +0000

    How many completed

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,22 +1,18 @@
 #!/usr/bin/node
-// Computes the number of tasks completed by user id
 
 const request = require('request');
-const finished = {};
-
 request(process.argv[2], function (error, response, body) {
   if (error) {
     console.log(error);
   } else if (response.statusCode === 200) {
-    const aux = JSON.parse(body);
-    for (let i = 0; i < aux.length; i++) {
-      finished[aux[i].userId] = 0;
-    }
-    for (let j = 0; j < aux.length; j++) {
-      if (aux[j].completed === true) {
-        finished[aux[j].userId] += 1;
+    const completed = {};
+    for (const task of JSON.parse(body)) {
+      if (task.completed === true) {
+        if (task.userId in completed) {
+          completed[task.userId]++;
+        } else { completed[task.userId] = 1; }
       }
     }
+    console.log(completed);
   }
-  console.log(finished);
 });
