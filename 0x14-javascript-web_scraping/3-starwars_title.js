a64abd11f7b21e60a7a3b9fcd62f546dcbd87727
dagute <1271@holbertonschool.com>
2020-05-12 18:16:14 +0000

    Star wars movie title

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const request = require('request');
+const movie = process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/' + movie;
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(JSON.parse(body).title);
+  }
+});
